2dfb3dec6c1a66063b2477d4dfd33bc36f3a0353
Didier Duhayon <didier.duhayon@gmail.com>
2018-03-14 04:59:29 +0100

    Adding devise and link Product to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# TODO: Destroy by hand (rails db:drop)
+# Product.destroy_all
+#User.destroy_all
 
-Product.destroy_all
+didier = User.create!(email: 'didier.duhayon@gmail.com', password: 'testtest')
 
-Product.create!(name: "Kudoz", url: "http://getkudoz.com", tagline: "Tinder for job search")
-Product.create!(name: "uSlide", url: "http://uslide.io", tagline: "Youtube sucks for education")
-Product.create!(name: "Medpics", url: "http://medpics.com", tagline: "Share your diagnostics")
+Product.create!(user: didier, name: "Kudoz", url: "http://getkudoz.com", tagline: "Tinder for job search",category: "tech")
+Product.create!(user: didier, name: "uSlide", url: "http://uslide.io", tagline: "Youtube sucks for education",category: "education")
+Product.create!(user: didier, name: "Medpics", url: "http://medpics.com", tagline: "Share your diagnostics",category: "tech")
